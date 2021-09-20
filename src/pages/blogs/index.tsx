2211d3f52905d0070a5d011b78bdad95ddc97afa
seanyasno <sean.yasno@gmail.com>
2021-09-20 21:59:27 +0300

    Started working on home page.

---
+++ src/pages/blogs/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+const BlogsPage: NextPage = () => {
+  return (
+    <>
+      <h1>Blogs Page</h1>
+    </>
+  );
+};
+
+export default BlogsPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,36 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import { Blog } from '@/types/index';
+import { Container, Typography, Box, Grid } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
-    <>Home Page</>
-  )
-}
+    <Box
+      sx={{
+        backgroundColor: '#1D212D',
+        color: '#EAE0D9',
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexFlow: 'column',
+        }}
+      >
+        <Typography variant='h2'>Sean Yasnogorodski</Typography>
+        <Typography variant='subtitle1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
