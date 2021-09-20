286cc3fc6a92f4ec312ba7992722a9ff21299f4b
seanyasno <sean.yasno@gmail.com>
2021-09-21 01:49:52 +0300

    Added new navbar.

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,27 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
+
+export const Navbar: React.FC = () => {
+  return (
+    <AppBar sx={{ height: '6vh' }} position='relative' elevation={0}>
+      <Toolbar sx={{ justifyContent: 'end' }}>
+        <Typography sx={{ fontWeight: 'light', marginRight: '30px' }}>
+          Home
+        </Typography>
+        <Link href='/blogs'>
+          <Typography
+            sx={{ fontWeight: 'light', marginRight: '30px', cursor: 'pointer' }}
+          >
+            Blog
+          </Typography>
+        </Link>
+        <Typography sx={{ fontWeight: 'light', marginRight: '30px' }}>
+          About
+        </Typography>
+        <Typography sx={{ fontWeight: 'light', marginRight: '30px' }}>
+          Contact
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,62 +4,103 @@ import { Blog } from '@/types/index';
 import { Container, Typography, Box, Grid, Button } from '@mui/material';
 import { theme } from 'src/consts/theme/theme';
 import { Instagram, LinkedIn, Place, Twitter } from '@mui/icons-material';
+import { Navbar } from '@/components/index';
 
 const Home: NextPage = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: '#1D212D',
-        width: '100%',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexFlow: 'column',
-      }}
-    >
-      <Box sx={{ display: 'flex', flex: 1 }}></Box>
-
+    <Box>
       <Box
         sx={{
-          display: 'flex',
+          backgroundColor: '#1D212D',
           width: '100%',
-          color: theme.palette.primary.contrastText,
+          height: '100vh',
+          display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           flexFlow: 'column',
-          flex: 1,
         }}
       >
-        <Typography variant='h1'>Sean Yasnogorodski</Typography>
-        <Typography variant='subtitle1'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </Typography>
-      </Box>
+        <Box
+          sx={{ display: 'flex', flex: 1, width: '100%', alignItems: 'start' }}
+        >
+          <Navbar />
+        </Box>
 
-      <Container
-        sx={{
-          width: '100%',
-          flex: 1,
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'end',
-          paddingBottom: '15px',
-        }}
-        maxWidth={false}
-      >
-        <Box sx={{ display: 'flex' }}>
-          <Place sx={{ color: '#877D61' }} />
-          <Typography sx={{ color: '#575A63', paddingLeft: '16px' }}>
-            Located in Israel
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            color: theme.palette.primary.contrastText,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexFlow: 'column',
+            flex: 1,
+          }}
+        >
+          <Typography variant='h1'>Sean Yasnogorodski</Typography>
+          <Typography variant='subtitle1'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </Typography>
         </Box>
-        <Box>
-          <Instagram sx={{ color: '#575A63' }} />
-          <LinkedIn sx={{ color: '#575A63', padding: '0 8px' }} />
-          <Twitter sx={{ color: '#575A63' }} />
-        </Box>
-      </Container>
+
+        <Container
+          sx={{
+            width: '100%',
+            flex: 1,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'end',
+          }}
+          maxWidth={false}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flex: 1,
+              paddingBottom: '40px',
+              paddingLeft: '30px',
+            }}
+          >
+            <Place sx={{ color: '#877D61' }} />
+            <Typography sx={{ color: '#575A63', paddingLeft: '16px' }}>
+              Located in Israel
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              flex: 1,
+              flexFlow: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography sx={{ color: '#877D61' }}>Scroll Down</Typography>
+            <Box
+              sx={{
+                backgroundColor: '#877D61',
+                height: '100px',
+                width: '1px',
+                marginTop: '30px',
+              }}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              flex: 1,
+              justifyContent: 'end',
+              paddingBottom: '40px',
+              paddingRight: '30px',
+            }}
+          >
+            <Instagram sx={{ color: '#575A63' }} />
+            <LinkedIn sx={{ color: '#575A63', padding: '0 16px' }} />
+            <Twitter sx={{ color: '#575A63' }} />
+          </Box>
+        </Container>
+      </Box>
     </Box>
   );
 };
