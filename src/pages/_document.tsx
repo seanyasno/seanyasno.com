abb12b41e852f2ef6a9944dca22e9c1d319eaf2e
Sean Yasnogorodski <sean.yasno@gmail.com>
2021-10-15 19:49:09 +0300

    Added pwa support.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Sean Yasnogorodski</title>
+
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='apple-touch-icon' href='/icon.png'></link>
+          <meta name='theme-color' content='#fff' />
+          <meta
+            name='description'
+            content="Sean Yasnogorodski's portfolio website."
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <Script
             async
